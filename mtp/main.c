d857d64966f493847890cf57ebb2954e08d1a2db
David Gartner <gartnerd@sephoris.lan>
2015-05-01 21:53:30 -0600

    Initial Commit

---
+++ mtp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mtp
+//
+//  Created by David Gartner on 2015-05-01.
+//  Copyright (c) 2015 David Gartner. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
